7e38f1797d11f576563a76e6a4e8a7ceb7226af9
Irfan Charania <irfan.charania@gmail.com>
2013-08-22 21:35:20 -0700

    Ignore promo links

--- resources/lib/post.py
+++ resources/lib/post.py
@@ -22,7 +22,9 @@ class Part():
 
 
 class Post():
-    __warningmsg = '{addon}: **WARNING** No {item} found for {url}'
+    __warningmsg = '{addon}: **WARNING** No {item} found for {text}'
+    regex_find_promo = re.compile('\([pP]romo\)')
+    regex_find_parts = re.compile('[pP]art\s*(\d+)')
 
     def __init__(self, matchstr):
         self.parts = {}
@@ -67,11 +69,15 @@ class Post():
         '''
         get part number from text.
         Single link is Part 0
+        Ignore 'promo' links
         '''
-        p = re.compile('[pP]art\s*(\d+)').findall(text)
-        if p:
-            return int(p[0])
-        return 0
+        if self.regex_find_promo.findall('Promo'):
+            return None
+        else:
+            p = self.regex_find_parts.findall(text)
+            if p:
+                return int(p[0])
+            return 0
 
     def add_link(self, posturl, urltext):
         '''
@@ -80,40 +86,46 @@ class Post():
         '''
         # parse text to get part number
         partnum = self.__get_part_number(urltext)
+        if partnum:
 
-        # parse posturl to get matchstr
-        match = self.__get_match_string(posturl)
-        if match:
-            print 'Match string: {match}'.format(match=match)
+            # parse posturl to get matchstr
+            match = self.__get_match_string(posturl)
+            if match:
+                print 'Match string: {match}'.format(match=match)
 
-            # get host from local match string dictionary
-            host, vidstr = self.__matchstr.get(match) or (None, None)
-            if host:
-                #print 'Host found: {host}, {vidstr}'.format(
-                #    host=host.label, vidstr=vidstr)
+                # get host from local match string dictionary
+                host, vidstr = self.__matchstr.get(match) or (None, None)
+                if host:
+                    #print 'Host found: {host}, {vidstr}'.format(
+                    #    host=host.label, vidstr=vidstr)
 
-                # get video id
-                vid = self.__get_video_id(posturl, vidstr)
-                if vid:
-                    #print 'Video ID: {vid}'.format(vid=vid)
+                    # get video id
+                    vid = self.__get_video_id(posturl, vidstr)
+                    if vid:
+                        #print 'Video ID: {vid}'.format(vid=vid)
 
-                    text = self.__get_part_text(partnum)
-                    #print 'part title: {text}'.format(text=text)
+                        text = self.__get_part_text(partnum)
+                        #print 'part title: {text}'.format(text=text)
 
-                    # add to parts dictionary
-                    part = self.parts.get(partnum, None)
-                    if part:
-                        ''' append media to existing part '''
-                        part.add_media(host, vid)
+                        # add to parts dictionary
+                        part = self.parts.get(partnum, None)
+                        if part:
+                            ''' append media to existing part '''
+                            part.add_media(host, vid)
+                        else:
+                            ''' add new part to new part number '''
+                            self.parts[partnum] = Part(partnum, host, vid, text)
                     else:
-                        ''' add new part to new part number '''
-                        self.parts[partnum] = Part(partnum, host, vid, text)
+                        print self.__warningmsg.format(
+                            addon=util.addon_id, item='video id', text=posturl)
                 else:
                     print self.__warningmsg.format(
-                        addon=util.addon_id, item='video id', url=posturl)
+                        addon=util.addon_id, item='host', text=posturl)
             else:
                 print self.__warningmsg.format(
-                    addon=util.addon_id, item='host', url=posturl)
+                    addon=util.addon_id, item='match', text=posturl)
         else:
             print self.__warningmsg.format(
-                addon=util.addon_id, item='match', url=posturl)
+                addon=util.addon_id, item='partnum',
+                text=urltext + ' - ' +  posturl)
+
